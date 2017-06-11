4ce3c0ab4b46626f1201252622d6f01c4c16c198
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2017-06-12 00:35:10 +0100

    Location module

---
+++ parsing/loc.ml
@@ -0,0 +1,39 @@
+open Lexing
+
+type t = {
+    loc_start: position;
+    loc_end: position;
+    loc_ghost: bool;
+  }
+
+let in_file name =
+  let loc = {
+      pos_fname = name;
+      pos_lnum = 1;
+      pos_bol = 0;
+      pos_cnum = -1;
+    }
+  in
+  { loc_start = loc;
+    loc_end = loc;
+    loc_ghost = true }
+
+let none = in_file "_none_"
+
+let make pos_start pos_end =
+  { loc_start = pos_start;
+    loc_end = pos_end;
+    loc_ghost = false }
+
+module Located = struct
+  type nonrec 'a t = {
+      loc_item: 'a;
+      loc_pos: t;
+    }
+
+  let lift elem pos_start pos_end =
+    { loc_item = elem;
+      loc_pos = make pos_start pos_end }
+
+  let item lc = lc.loc_item
+end
