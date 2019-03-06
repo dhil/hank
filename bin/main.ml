1ccc19f7447e308253b7855d207f913dfabac137
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2019-03-06 17:12:06 +0000

    Consume stdin when no files were given.

--- bin/main.ml
+++ bin/main.ml
@@ -8,16 +8,18 @@ let usage = "usage: hank <source>"
 
 let _ =
   Arg.parse cmd_args (fun f -> Settings.source_files := f :: !Settings.source_files) usage;
-  match !Settings.source_files with
-  | [] -> print_endline usage
-  | _ ->
-     List.iter
-       (fun source ->
-         let ic = open_in source in
-         try
-           let lexbuf = Lexing.from_channel ic in
-           let tokens = Hank.Lexer.tokenise lexbuf in
-           close_in ic;
-           List.iter (fun t -> print_endline (Hank.Token.to_string t)) tokens
-         with e -> close_in_noerr ic; raise e)
-       !Settings.source_files
+  let () =
+    match !Settings.source_files with
+    | [] -> Settings.source_files := ["/dev/stdin"]
+    | _ -> ()
+  in
+  List.iter
+    (fun source ->
+      let ic = open_in source in
+      try
+        let lexbuf = Lexing.from_channel ic in
+        let tokens = Hank.Lexer.tokenise lexbuf in
+        close_in ic;
+        List.iter (fun t -> print_endline (Hank.Token.to_string t)) tokens
+      with e -> close_in_noerr ic; raise e)
+    !Settings.source_files
